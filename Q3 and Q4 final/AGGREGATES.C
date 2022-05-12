eddbd53d82a25289663ba49d0a980d4c37dc821f
mariaghobrial <91097128+mariaghobrial@users.noreply.github.com>
2022-05-11 23:44:40 -0400

    Create AGGREGATES.C

---
+++ Q3 and Q4 final/AGGREGATES.C
@@ -0,0 +1,103 @@
+//
+//  AGGREGATE.c
+//  a1.comp348
+//
+//  Created by Maria Ghobrial on 2022-05-11.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "AGGREGATE.H"
+
+//#include "AGGREGATE.H"
+
+//fct that returns min element in an array
+float min(float arr[], int size){
+    if (arr == NULL || size <0){
+           printf("FATAL ERROR IN LINE %d\n", __LINE__);
+          // printf("program terminated", _ab);
+
+
+       }
+
+        float temp=arr[0];
+
+        for (int i=0; i<size;i++){
+            if(temp>arr[i]) {
+               temp=arr[i];
+             }
+
+        }
+        printf("The min is: %f\n",temp);
+        return temp;
+
+}
+//fct that returns max element in an array
+float max(float arr[], int size){
+    if (arr == NULL || size <0){
+           printf("FATAL ERROR IN LINE %d\n", __LINE__);
+          // printf("program terminated", _ab);
+
+
+       }
+
+    float temp=arr[0];
+
+    for (int i=0; i<size;i++){
+        if(temp<arr[i]) {
+         temp=arr[i];
+      }
+
+    }
+    printf("The max is: %f\n",temp);
+    return temp;
+}
+//fct that returns the sum of the elements in an array
+float sum(float arr[], int size) {
+    if (arr == NULL || size <0){
+           printf("FATAL ERROR IN LINE  %d\n", __LINE__);
+          // printf("program terminated", _ab);
+
+
+       }
+
+     int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    printf("The sum is: %d\n",sum);
+    return sum;
+}
+//fct that returns the sum of the elements in an array
+float avg(float arr[], int size){
+    if (arr == NULL || size <0){
+           printf("FATAL ERROR IN LINE %d\n", __LINE__);
+          // printf("program terminated", _ab);
+
+
+       }
+
+    int sum = 0;
+    for (int i=0; i<size; i++)
+       sum += arr[i];
+    
+    
+    printf("The average is: %d\n",sum/size);
+    return sum/size;
+}
+
+   
+
+//fct that returns the avg of max and min of an array
+float pseudo_avg(float arr[], int size){
+    if (arr == NULL || size <0){
+           printf("FATAL ERROR IN LINE  %d\n", __LINE__);
+          // printf("program terminated", _ab);
+       }
+
+    
+    float avg= max(arr, size)+ min(arr,size);
+    printf("The pseudo avg is: %f\n",avg/2);
+    return avg/2;
+    
+}
