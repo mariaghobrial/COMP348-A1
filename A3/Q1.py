5c44ac7640f1a8865b131c6286a51eb7f1f372db
mariaghobrial <91097128+mariaghobrial@users.noreply.github.com>
2022-06-04 22:33:47 -0400

    Update Q1.py

--- A3/Q1.py
+++ A3/Q1.py
@@ -1,31 +1,32 @@
 class Q1:
 
-    
+
 
    #this fct returns the lucas seq
-    def lucas1(self,n):
+    def lucas1(self, n):
         luc=[2,1]
         for i in range(2, n+1):
             luc.append(luc[-1]+luc[-2])
         return luc
-   
+
     #this fct prints all the number in the seq less than n
     def lu(self, n):
         lst = self.lucas1(n)
         lastelt = int(lst[len(lst)-1])
 
-        while lastelt > n:
+        while lastelt >= n:
             lst.pop()
             lastelt = int(lst[len(lst)-1]) #need to update the value of lastelt
             if lastelt < n:
                 pass
-    
+
         return lst
 
 
 if __name__ == '__main__':
 
      test=Q1()
-     print(test.lucas1(5))
+    #print(test.lucas1(5))
      print (test.lu(50))
+     print(test.lu(4))
 
